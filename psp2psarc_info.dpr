5086dd7d2a0d9529b4c52af54b01dda368ded7cd
Yoti <realyoti@gmail.com>
2018-08-28 02:06:03 +0300

    psp2psarc_info v1.1

--- psp2psarc_info.dpr
+++ psp2psarc_info.dpr
@@ -15,7 +15,7 @@ asm
   bswap eax
 end;
 
-begin // psp2psarc_info by Yoti
+begin // psp2psarc_info v1.1 by Yoti
   try
     if (ParamCount < 1) then begin
       WriteLn('Error: no input file (-1)');
@@ -25,48 +25,52 @@ begin // psp2psarc_info by Yoti
       WriteLn('Error: input file not exists (-2)');
       Halt(2);
     end;
+//    if (FileSize(ParamStr(1) < $20) then begin
+//      WriteLn('Error: input file too small (-?)');
+//      Halt(?);
+//    end;
     inFS:=TFileStream.Create(ParamStr(1), fmOpenRead or fmShareDenyNone);
-      inFS.Read(Rec, SizeOf(Rec));
-      if (Rec <> $52415350) then begin
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
+      if (Rec <> $50534152) then begin
         WriteLn('Error: input file not a PSAR (-3)');
         inFS.Free;
         Halt(3);
       end;
-      WriteLn('magic: 0x' + IntToHex(Swap(Rec), 8));
-      inFS.Read(Rec, SizeOf(Rec));
-      WriteLn('version: 0x' + IntToHex(Swap(Rec), 8)
-      + ' (v' + IntToHex(Word(Swap(Rec) shr 16), 1) + '.' + IntToHex(Word(Swap(Rec) shr 0), 1) + ')');
-      inFS.Read(Rec, SizeOf(Rec));
+      WriteLn('magic: 0x' + IntToHex(Rec, 8));
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
+      WriteLn('version: 0x' + IntToHex(Rec, 8)
+      + ' (v' + IntToHex(Word(Rec shr 16), 1) + '.' + IntToHex(Word(Rec shr 0), 1) + ')');
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
       Write('compression_type: ');
-      case Swap(Rec) of
+      case Rec of
         $7A6C6962:
           WriteLn('zlib');
         $6C7A6D61:
           WriteLn('lzma');
-        else
-          WriteLn('0x' + IntToHex(Swap(Rec), 8)); // error
+        else // error
+          WriteLn('0x' + IntToHex(Rec, 8));
       end;
-      inFS.Read(Rec, SizeOf(Rec));
-      WriteLn('toc_length: 0x' + IntToHex(Swap(Rec), 8) + ' (' + IntToStr(Swap(Rec))+ ')');
-      inFS.Read(Rec, SizeOf(Rec));
-      WriteLn('toc_entry_size: 0x' + IntToHex(Swap(Rec), 8) + ' (' + IntToStr(Swap(Rec))+ ')');
-      inFS.Read(Rec, SizeOf(Rec));
-      WriteLn('toc_entries: 0x' + IntToHex(Swap(Rec), 8) + ' (1+' + IntToStr(Swap(Rec)-1)+ ')');
-      inFS.Read(Rec, SizeOf(Rec));
-      WriteLn('block_size: 0x' + IntToHex(Swap(Rec), 8) + ' (' + IntToStr(Swap(Rec))+ ')');
-      inFS.Read(Rec, SizeOf(Rec));
-      Write('archive_flags: 0x' + IntToHex(Swap(Rec), 8) + ' (');
-      case Swap(Rec) of
-        0:
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
+      WriteLn('toc_length: 0x' + IntToHex(Rec, 8) + ' (' + IntToStr(Rec)+ ')');
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
+      WriteLn('toc_entry_size: 0x' + IntToHex(Rec, 8) + ' (' + IntToStr(Rec)+ ')');
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
+      WriteLn('toc_entries: 0x' + IntToHex(Rec, 8) + ' (1+' + IntToStr(Rec-1)+ ')');
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
+      WriteLn('block_size: 0x' + IntToHex(Rec, 8) + ' (' + IntToStr(Rec)+ ')');
+      inFS.Read(Rec, SizeOf(Rec)); Rec:=Swap(Rec);
+      Write('archive_flags: 0x' + IntToHex(Rec, 8) + ' (');
+      case Rec of
+        $00000000:
           Write('relative');
-        1:
+        $00000001:
           Write('ignorecase');
-        2:
+        $00000002:
           Write('absolute');
-        3:
+        $00000003:
           Write('ignorecase+absolute');
         else // error
-          Write('0x' + IntToHex(Swap(Rec), 8));
+          Write('0x' + IntToHex(Rec, 8));
       end;
       WriteLn(')');
     inFS.Free;
