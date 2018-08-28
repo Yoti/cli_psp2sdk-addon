e0513d6ea88ccca14dc967363633129d2db8f410
Yoti <realyoti@gmail.com>
2018-08-28 20:41:47 +0300

    psp2psarc_create v1.1

--- psp2psarc_create.bat
+++ psp2psarc_create.bat
@@ -1,9 +1,12 @@
 @echo off
-rem psp2psarc_create by Yoti
+rem psp2psarc_create v1.1 by Yoti
 title Wait
 if "%1"=="" (
 	echo Error: no XML
 	goto thisistheend
+)
+for %%i in (%~n1.*) do (
+	if not "%%i"=="%~n1.xml" del /q %%i
 )
 psp2psarc.exe --xml=%1
 :thisistheend
