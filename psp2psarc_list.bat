8c26d543928c60297b7350b350a81dd4fedeeae7
Yoti <realyoti@gmail.com>
2018-08-28 20:21:38 +0300

    psp2psarc_list v1.1

--- psp2psarc_list.bat
+++ psp2psarc_list.bat
@@ -1,8 +1,10 @@
 @echo off
-rem psp2psarc_list by Yoti
+rem psp2psarc_list v1.1 by Yoti
+title Wait
 if "%1"=="" (
 	echo Error: no PSARC
 	goto thisistheend
 )
 psp2psarc.exe list %~nx1 > %~n1_list.txt
 :thisistheend
+title Done
