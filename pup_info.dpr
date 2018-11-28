e60e7a5b6f154e2ff1244325072eb127a6b18297
Yoti <realyoti@gmail.com>
2018-11-28 15:14:13 +0300

    pup_info v0.1

---
+++ pup_info.dpr
@@ -0,0 +1,154 @@
+program pup_info;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Classes,
+  SysUtils;
+
+var
+  b: Byte;
+  i: Integer;
+  j: Integer;
+  k: Integer;
+  ID8: UInt64;
+  Size8: UInt64;
+  Offset8: UInt64;
+  Record8: UInt64;
+  bOffset: UInt64;
+  outName: String;
+  outPath: String;
+  FileCount8: UInt64;
+  inMS: TMemoryStream;
+  HeaderLength8: UInt64;
+  Hash: Array[0..31] of Byte;
+{
+function Swap32(I: UInt32): UInt32;
+asm
+  BSWAP EAX
+end;
+}
+function Swap64(I: UInt64): UInt64;
+asm
+  MOV   EDX,I.Int64Rec.Lo
+  BSWAP EDX
+  MOV   EAX,I.Int64Rec.Hi
+  BSWAP EAX
+end;
+
+function IntToStrEx(i: Integer; j: Integer): String;
+var
+  s: String;
+  k: Integer;
+begin
+  s:=IntToStr(i);
+  if Length(s) < j then begin
+    for k:=0 to j - Length(s) - 1
+    do s:='0' + s;
+  end;
+  IntToStrEx:=s;
+end;
+{
+procedure SaveFromMsViaFs(FromMs: TMemoryStream; Name: String; Offset, Size: Cardinal);
+var
+  TempOffset: Cardinal;
+  OutputFile: TFileStream;
+begin
+  OutputFile:=TFileStream.Create(Name, fmCreate);
+  TempOffset:=FromMs.Position;
+  FromMs.Seek(Offset, soFromBeginning);
+    OutputFile.CopyFrom(FromMs, Size);
+  FromMs.Seek(TempOffset, soFromBeginning);
+  OutputFile.Free;
+end;
+}
+begin
+  k:=0;
+  outName:='';
+  outPath:='';
+  FillChar(Hash, SizeOf(Hash), $00);
+
+  WriteLn('pup_info by Yoti');
+  if ParamCount < 1 then Exit;
+  if FileExists(ParamStr(1)) = False then Exit;
+
+  inMS:=TMemoryStream.Create;
+  inMS.LoadFromFile(ParamStr(1));
+  inMS.Read(Record8, SizeOf(Record8));
+  if Record8 = Swap64($5343455546000001) then begin
+    WriteLn('Magic signature: ' + IntToHex(Swap64(Record8), SizeOf(Record8)*2) + ' (valid)');
+    inMS.Read(Record8, SizeOf(Record8));
+    WriteLn('Package version: ' + IntToHex(Record8, SizeOf(Record8)*2));
+    inMS.Read(Record8, SizeOf(Record8));
+    WriteLn('Image version:   ' + IntToHex(Record8, SizeOf(Record8)*2));
+    inMS.Read(FileCount8, SizeOf(FileCount8));
+    WriteLn('File count:      ' + IntToHex(FileCount8, SizeOf(FileCount8)*2));
+    inMS.Read(HeaderLength8, SizeOf(HeaderLength8));
+    WriteLn('Header length:   ' + IntToHex(HeaderLength8, SizeOf(HeaderLength8)*2));
+    inMS.Read(Record8, SizeOf(Record8));
+    WriteLn('Package length:  ' + IntToHex(Record8, SizeOf(Record8)*2));
+
+    inMS.Seek($50, soCurrent);
+
+    for i:=1 to FileCount8 do begin
+      inMS.Read(ID8, SizeOf(ID8));
+      Write(IntToHex(ID8, SizeOf(ID8)*2));
+      Write(' ');
+      inMS.Read(Offset8, SizeOf(Offset8));
+      Write(IntToHex(Offset8, SizeOf(Offset8)*2));
+      Write(' ');
+      inMS.Read(Size8, SizeOf(Size8));
+      Write(IntToHex(Size8, SizeOf(Size8)*2));
+      inMS.Read(Record8, SizeOf(Record8)); // unknown
+
+      case ID8 of
+        $0100: outName:='version.txt';
+        $0101: outName:='license.xml';
+        $0200: outName:='guiupd.self';
+        $0204: outName:='cuiupd.self';
+        $0300..$03FF: begin
+          Inc(k);
+          outName:='dat' + IntToStrEx(k, 4) + '.pkg';
+        end;
+        $0400: outName:='pkgscewm.wm';
+        $0401: outName:='pkgsceas.as';
+        $2005: outName:='cpupold.bin';
+        $2006: outName:='cpupnew.bin';
+        else outName:='dat' + IntToHex(ID8, 4) + '.unk';
+      end;
+      Write(' ' + outName);
+      if ID8 = $0100 then begin
+        Write(' ');
+        bOffset:=inMS.Position;
+        inMS.Seek(Offset8, soFromBeginning);
+        for j:=1 to Size8-1 do begin
+          inMS.Read(b, 1);
+          Write(Chr(b));
+        end;
+        inMS.Seek(bOffset, soFromBeginning);
+      end;
+      WriteLn;
+      // SaveFromMsViaFs
+    end;
+
+    for i:=1 to FileCount8 do begin
+      inMS.Read(Record8, SizeOf(Record8));
+      Write(IntToHex(Record8, SizeOf(Record8)*2));
+      FillChar(Hash, SizeOf(Hash), $00);
+      inMS.Read(Hash, $20);
+      Write(' ');
+      for j:=0 to SizeOf(Hash)-1 do begin
+        Write(IntToHex(Ord(Hash[j]), 2));
+      end;
+      WriteLn;
+      inMS.Seek($18, soCurrent); // unknown
+    end;
+
+    for j:=1 to 4 do begin
+      inMS.Read(Record8, SizeOf(Record8));
+      Write(IntToHex(Record8, SizeOf(Record8)*2) + ' ');
+    end;
+    WriteLn;
+  end;
+  inMS.Free;
+end.
